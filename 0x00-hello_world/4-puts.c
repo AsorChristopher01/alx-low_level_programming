d85b6cb8c6aa1eb1fe0b699e00067f19ef413304
root <asorchristopher247@gmail.com>
2022-09-08 15:08:41 -0700

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *  *main- Serves as the entry point for the program
- ii*   *
- *    *Return: By default returns zero if no erros otherwise
- *    returns non-zero value.
- *    
- */
+*main -Serves as the entry point of the program
+*
+* *Return - By default returns zero if no errors 
+* otherwise returns non-zero value.
+* */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-i}
+	puts("\"Programming is like building a
+			multilingual puzzle");
+	return (0);
+}
