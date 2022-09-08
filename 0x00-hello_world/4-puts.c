ed974f27971c4584576a55d49673115dc368d349
root <asorchristopher247@gmail.com>
2022-09-08 15:16:23 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  *   *
  *    *Return: By default returns zero if no erros otherwise returns 
  *    non-zero value.
- *     */
+ *   
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
