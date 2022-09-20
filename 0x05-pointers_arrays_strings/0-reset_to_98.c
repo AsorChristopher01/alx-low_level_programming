0802c56b8b44289f1227ccb88d85b9d4969d2b0d
AsorChristopher01 <asorchristopher247@gmail.com>
2022-09-20 13:01:35 +0100

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,10 +9,13 @@
 int main(void)
 {
     int n;
+    n = 90;
+    int *var;
+    *var = &n;
 
-    n = 402;
     printf("n=%d\n", n);
     reset_to_98(&n);
     printf("n=%d\n", n);
     return (0);
+
 }
