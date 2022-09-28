6ae1a6bb05990956c047167ef334e7238b2ee451
AsorChristopher01 <asorchristopher247@gmail.com>
2022-09-29 00:10:25 +0100

    m

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function to do pow recursion
+ * @x: interger
+ * @y: power
+ * Return: Always 0
+ */
+int _pow_recursion(int x, int y);
+{
+	if (y < 0)
+		return (-1);
+	return _pow_recursion(x, y);
+}
