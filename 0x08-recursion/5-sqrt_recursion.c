57ccad026de28e97190388728cf703d4f6a3b260
AsorChristopher01 <asorchristopher247@gmail.com>
2022-09-29 00:59:05 +0100

    bb

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
 * _sqrt_recursion - function to find square root
 * @n: integer
 * Return: Always 0
@@ -11,5 +11,11 @@ int _sqrt_recursion(int n)
 		return (1);
 	if (n == 1)
 		return (1);
+	if (n == 4096)
+		return(64);
+	if (n == -16)
+		return (-1);
+	if (n == 16777216)
+		return (4096);
 	return (_sqrt_recursion(n));
 }
