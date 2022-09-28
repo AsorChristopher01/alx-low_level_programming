2f2e22acad95378cffebe04c58142aed208d1fe0
AsorChristopher01 <asorchristopher247@gmail.com>
2022-09-29 00:54:16 +0100

    g

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+i#include "main.h"
+
+/** 
+* _sqrt_recursion - function to find square root
+* @n: integer
+* Return: Always 0
+*/
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n == 1)
+		return (1);
+	return (_sqrt_recursion(n));
+}
