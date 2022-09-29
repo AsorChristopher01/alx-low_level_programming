b9fa2b329455dc9433cc83ba60b899bdef62130f
AsorChristopher01 <asorchristopher247@gmail.com>
2022-09-29 01:05:19 +0100

    mm

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/** 
+* is_prime_number - function to find prime number
+* @n:integer
+* Return: Always 0
+*/
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (1);
+	return (is_prime_number(n));
+}
