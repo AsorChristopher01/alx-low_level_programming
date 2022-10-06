db202c6902a89a67f5e60113fa0adfefab3978ee
AsorChristopher01 <asorchristopher247@gmail.com>
2022-10-06 23:35:18 +0100

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,10 +3,15 @@
 #include <stdio.h>
 
 /**
-  * _realloc - ...
-  * @ptr: ...
-  * @old_size: ...
-  * @new_size: ...
-  *
-  * Return: ...
-  */
+* @mul:function to multiply
+* @num1: first number
+* @num2: second number
+* Return:Always 0
+*/
+int mul(num1, num2)
+	return (num1 * num2)
+	if (typeof(num1) && typeof(num2) != int)
+	{
+		print("%d\n", "Error")
+			return (98)
+	}
