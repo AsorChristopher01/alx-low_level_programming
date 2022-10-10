abd0bd5d8edd62fecc53b77970ba49041a474045
AsorChristopher01 <asorchristopher247@gmail.com>
2022-10-10 10:20:27 +0100

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ *@owner: Third memeber
+ * Description: Longer description
+ */
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+	
+};
